9de2a620a7af2bab71b05d333f6d422f148f5a14
nayunn <104406100+nayunn@users.noreply.github.com>
2025-09-15 01:46:30 +0900

    [Bronze III] Title: 공, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1547. 공/공.py
+++ 백준/Bronze/1547. 공/공.py
@@ -4,18 +4,13 @@ cup =[1,0,0]
 
 for i in range (m):
     move1, move2 = map(int, input().split())
-
-    #print("시도 : " , i+1) #확인용 코드
-    
     if cup[move1-1]==1 or cup[move2-1]==1:
-        cup[move1-1], cup[move2-1] = cup[move2-1], cup[move1-1]
-
-    ''' 
-    #확인용 코드       
-    for j in range(len(cup)):
-        if cup[j] == 1:
-            print("현재 공 위치 : ", j+1 )
-            print (cup) '''
+        if cup[move1-1] ==1:
+            cup[move1-1] = 0
+            cup[move2-1] = 1
+        else: 
+            cup[move2-1] = 0
+            cup[move1-1] = 1
 
 for i in range (len(cup)):
     if cup[i] ==1:
