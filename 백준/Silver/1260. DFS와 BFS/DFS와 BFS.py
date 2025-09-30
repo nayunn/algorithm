029840a7f8b1b0cf0cf06f9f43e514cd01e89526
nayunn <104406100+nayunn@users.noreply.github.com>
2025-09-30 23:11:40 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 35676 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,34 +1,53 @@
+import sys
 from collections import deque
 
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+# 1. 입력 받기
 n, m, v = map(int, input().split())
-graph = {i: [] for i in range(1, n+1)} 
+graph = {i: [] for i in range(1, n+1)}
 
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-def DFS(node, visited):
-    visited.add(node)
-    print(node, end=" ")
-    for nxt in sorted(graph[node]): 
-        if nxt not in visited:
-            DFS(nxt, visited)
+# 2. 인접 리스트 정렬 (작은 번호 우선 탐색 위해)
+for i in range(1, n+1):
+    graph[i].sort()
 
-def BFS(start):
-    visited = set()
-    q = deque([start])
+# 3. DFS 구현
+visited_dfs = [False] * (n + 1)
+dfs_order = []
 
+def dfs(node):
+    visited_dfs[node] = True
+    dfs_order.append(node)
+    for nxt in graph[node]:
+        if not visited_dfs[nxt]:
+            dfs(nxt)
+
+# 4. BFS 구현
+visited_bfs = [False] * (n + 1)
+bfs_order = []
+
+def bfs(start):
+    q = deque()
+    q.append(start)
+    visited_bfs[start] = True
     while q:
         node = q.popleft()
-        if node in visited:
-            continue
-        visited.add(node)
-        print(node, end=" ")
-        for nxt in sorted(graph[node]):
-            if nxt not in visited:
+        bfs_order.append(node)
+        for nxt in graph[node]:
+            if not visited_bfs[nxt]:
+                visited_bfs[nxt] = True
                 q.append(nxt)
 
-DFS(v, set())
-print()
-BFS(v)
+# 5. 탐색 실행
+dfs(v)
+bfs(v)
+
+# 6. 출력
+print(*dfs_order)
+print(*bfs_order)
